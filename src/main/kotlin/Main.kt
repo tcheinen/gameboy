ac4d0450b8bee236ab4c2333c8184e9f08312886
Teddy Heinen <skyshayde@gmail.com>
2019-03-27 14:06:04 -0500

    libgdx stuff and rom loading

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,24 +1,27 @@
 package com.teddyheinen
 
 import com.badlogic.gdx.Game
+import com.badlogic.gdx.backends.lwjgl.*
 import cpu.Cpu
+import java.io.File
 
 @ExperimentalUnsignedTypes
 fun main(args: Array<String>) {
-    val filename = "roms/"
-    val cpu: Cpu = Cpu()
-    while (true) {
-        cpu.tick()
-//        Thread.sleep(100)
-    }
+//    val filename = "roms/cpu_instrs/individual/06-ld r,r.gb"
+//    val cpu: Cpu = Cpu()
+//    cpu.mmu.loadFile(File(filename))
+//    while (true) {
+//        cpu.tick()
+////        Thread.sleep(100)
+//    }
+    val application = LwjglApplicationConfiguration()
+    application.title = "Gameboy"
+    LwjglApplication(Main(), application)
 }
 
 class Main : Game() {
-
-
-
     override fun create() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        println("")
     }
 
 }
